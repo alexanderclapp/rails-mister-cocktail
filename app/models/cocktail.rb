fd0c9159de711d2154669b9d24d99b4787c1eff0
Alexander Clapp <alexanderclapp@gmail.com>
2017-11-17 17:51:04 -0200

    fixed formatting issues

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,5 @@ has_many :ingredients, through: :doses
 has_attachment :photo
 has_attachment :background_photo
 validates :name, presence: true, uniqueness: true, allow_blank: false
+validates :background_photo, presence: true
 end
